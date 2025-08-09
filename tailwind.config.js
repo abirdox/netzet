449670dec34b13f04dabfdd02ff35f55cb0350ee
Abir Hosen <abir71.hosen@gmail.com>
2025-08-09 13:01:03 +0600

    tailwind config font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
@@ -8,11 +9,14 @@ module.exports = {
         secondary: "#10CBE0",
         semi_secondary: "#00E7F9",
         gray: "#ABABAB",
-        semi_gray: "#A9A9A9"
+        semi_gray: "#A9A9A9",
       },
-      screens: {
-        sm390: "390px",
+      fontFamily: {
+        urbanist: ['var(--font-urbanist)', 'sans-serif'],
+        figtree: ['var(--font-figtree)', 'sans-serif'],
       },
+      fontSize: {},
+      screens: {},
     },
   },
   plugins: [],
